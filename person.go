bfd12e02941a2e71153895994ac1021d0f5c9693
Marco Troisi <hello@marcotroisi.com>
2016-02-10 09:52:55 +0100

    person

--- person.go
+++ person.go
@@ -1,12 +1,34 @@
 package champ
 
-type Person interface {
+type PersonInterface interface {
+	Id() int
 	FirstName() string
 	LastName() string
 	Email() string
 }
 
-type People interface {
+type PeopleInterface interface {
 	Add(firstname string, lastname string, email string)
 	List() []Person
+}
+
+type Person struct {
+	id                         int
+	firstName, lastName, email string
+}
+
+func (p *Person) Id() int {
+	return p.id
+}
+
+func (p *Person) FirstName() string {
+	return p.firstName
+}
+
+func (p *Person) LastName() string {
+	return p.lastName
+}
+
+func (p *Person) Email() string {
+	return p.email
 }
